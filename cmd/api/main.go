6c7f2bdc9cb59c35f514f6a2053c2ad543e5f011
Yor-Jihons <106572468+Yor-Jihons@users.noreply.github.com>
2026-03-11 21:58:28 +0900

    feat: APIキーを使った方法に切り替えた

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,18 @@ import (
 	"os"
 
 	"github.com/Yor-Jihons/portal-api/internal/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// .envファイルを読み込む (ファイルがなくてもエラーにしない)
+	_ = godotenv.Load()
+
 	r := routes.SetupRouter()
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
 	r.Run(":" + port)
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,17 +2,21 @@ package routes
 
 import (
 	"github.com/Yor-Jihons/portal-api/internal/handlers"
+	"github.com/Yor-Jihons/portal-api/internal/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	// study-histories関連のルート
-	personGroup := r.Group("/study-histories")
+	// CORS設定（VercelのURLからのアクセスのみ許可する設定をここに追加）
+
+	// グループ全体に認証をかける
+	authGroup := r.Group("/study-histories")
+	authGroup.Use(middlewares.ApiKeyAuth()) // このグループ以下の全ルートに門番を設置
 	{
-		personGroup.GET("", handlers.GetStudyHistories)
-		personGroup.POST("", handlers.CreateStudyHistory)
+		authGroup.GET("", handlers.GetStudyHistories)
+		authGroup.POST("", handlers.CreateStudyHistory)
 	}
 
 	return r
